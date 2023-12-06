b98a04ef610f584272506278f897982c92f31b25
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-06 18:17:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+  shortHex = shortHex.replace("#", "").toUpperCase();
+
+	if (!/^[0-9A-F]{3}$/.test(shortHex)) {
+    throw new Error("Invalid short hex code");
+  }
+
+	 const fullHex = shortHex
+    .split("")
+    .map((char) => char.repeat(2))
+    .join("");
+
+  return `#${fullHex}`;
+	
 };
 
 // Do not change the code below.
